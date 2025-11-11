13ccbfea79562b5ef65ce1af321548a01b078d00
khoussngom <khoussn@gmail.com>
2025-11-11 10:16:47 +0000

    qr

--- src/main/java/com/khouss/UsersMicroservice/services/SmsServiceImpl.java
+++ src/main/java/com/khouss/UsersMicroservice/services/SmsServiceImpl.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-
-
 @Service
 public class SmsServiceImpl implements SmsService {
 
